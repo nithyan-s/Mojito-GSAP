102084a79dc6892e3438de00e18e8a9f349620fb
nithyan-s <nithyan.4417@gmail.com>
2025-09-29 22:52:03 +0530

    setup files added

---
+++ gsap/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { navLinks } from '../../constants/index.js'
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <a href="#home" className="flex items-center gap-2">
+                <p>Velvet Pour</p>
+            </a>
+
+            <ul>
+                {navLinks.map((link) => (
+                    <li key={link.div}>
+                        <a href={`#${link.id}`}>{link.title}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
