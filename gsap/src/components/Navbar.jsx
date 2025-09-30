5e193c581fb7bad3fd16180f8bccc163fe0e0c7c
nithyan-s <nithyan.4417@gmail.com>
2025-10-01 00:29:07 +0530

    hero page completed

--- gsap/src/components/Navbar.jsx
+++ gsap/src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
 
             <ul>
                 {navLinks.map((link) => (
-                    <li key={link.div}>
+                    <li key={link.id}>
                         <a href={`#${link.id}`}>{link.title}</a>
                     </li>
                 ))}
